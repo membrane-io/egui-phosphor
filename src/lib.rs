16b8094388a56d9d674f2ad28fea6eafde1d213a
Juan Campa <juancampa@gmail.com>
2024-11-30 15:52:50 -0500

    Use Arc<FontData> to make it work with the upcoming egui version

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 pub mod variants;
+use std::sync::Arc;
 pub use variants::*;
 
 pub fn add_to_fonts(fonts: &mut egui::FontDefinitions, variant: Variant) {
-    fonts
-        .font_data
-        .insert("phosphor".into(), variant.font_data());
+  fonts.font_data.insert("phosphor".into(), Arc::new(variant.font_data()));
 
-    if let Some(font_keys) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
-        font_keys.insert(1, "phosphor".into());
-    }
+  if let Some(font_keys) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
+    font_keys.insert(1, "phosphor".into());
+  }
 }
